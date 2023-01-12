66497df126048e9a08bcda9fe1448bfae07f2e28
Patrick Pata <patrickrpata@gmail.com>
2023-01-12 12:12:44 -0800

    Added R package check and download code
    -New function file loads/downloads required R packages needed for the project.
    -Removed redundant functions between QOL and zoop_traits toolkits.
    -Updated scripts for sourcing the function files.

---
+++ functions/R_package_check.R
@@ -0,0 +1,52 @@
+# R packages check and download
+# Created by P. Pata
+# Last modified January 12, 2023
+#
+# This checks and loads the local library if the required packages for this 
+# R project are present. If not, the packages are downloaded from CRAN.
+# 
+# prepareLibrary() function calls the package.check scripts and loads all 
+# required libraries.
+
+packages <-
+  c("tidyverse",
+    "vegan",
+    "lubridate",
+    "lmodel2",
+    "openxlsx",
+    "here",
+    "knitr",
+    "PBSmapping",
+    "cowplot",
+    "viridis",
+    "Polychrome")
+
+package.check <- lapply(
+  packages,
+  FUN = function(x)
+  {
+    if (!require(x, character.only = TRUE))
+    {
+      install.packages(x, dependencies = TRUE,
+                       repos = "http://cran.us.r-project.org")
+      library(x, character.only = TRUE)
+    }
+  }
+)
+rm("package.check")
+
+prepareLibrary <- function(){
+  package.check <- lapply(
+    packages,
+    FUN = function(x)
+    {
+      if (!require(x, character.only = TRUE))
+      {
+        install.packages(x, dependencies = TRUE,
+                         repos = "http://cran.us.r-project.org")
+        library(x, character.only = TRUE)
+      }
+    }
+  )
+  
+}
--- functions/zoop_traits_toolkit.R
+++ functions/zoop_traits_toolkit.R
@@ -12,10 +12,6 @@
 #
 # ** Data wrangling and curation functions **
 #
-# geomean(x): Calculates the geometric mean of an array.
-# scaleFun(x): Returns a number with two decimal places.
-# standard_temp(rate0, t0, t, Q10): Calculate the value of a rate to a reference
-#   temperature using a Q10 coefficient.
 # convertRateT2WS(df, sizeAssoc, trtName1, trtName2, trtUnit):
 #   Converts a by individual rate to a weight-specific rate.
 # standardizeTable(file.list, s.format, taxonomy, lifestagelist, trait.directory):
@@ -24,11 +20,6 @@
 #   Converts percent composition value to total bulk composition value.
 # convertTotal2PW(df, sizeAssoc, trtName1, trtName2, trtUnit = "percent"):
 #   Converts total bulk composition to percent composition relative to a weight.
-# cleanStrings(A): Cleans spaces, NAs, and blanks from strings for notes and 
-#   references. Also removes duplicated strings separated by ;.
-# cleanStringsList(A): Apply cleanStrings() to a list.
-# cleanScientificName(name): Cleans the verbatim scientific name from trailing 
-#   spaces and life stage information. Also removes the sp, aff, and cf texts.
 # assignMajorGroup(taxonomy): Assigns the zooplankton major group in a taxonomy
 #   data frame.
 # getMaxObsNum(tN, tI, df): Gets the maximum observation number for a trait and 
@@ -66,13 +57,6 @@
 
 require(tidyverse)
 require(lmodel2)
-
-`%notin%` <- Negate(`%in%`)
-geomean <- function(x) {  exp(mean(log(x), na.rm = TRUE))  }
-scaleFUN <- function(x) sprintf("%.2f", x)
-standard_temp <- function(rate0, t0, t = 15, Q10 = 2.8) {
-  10^(log10(rate0) + log10(Q10)*((t-t0)/10) )
-}
 
 # For converting individual rates to weight specific
 convertRateT2WS <- function(df, sizeAssoc, trtName1, trtName2, trtUnit) {
@@ -140,42 +124,6 @@ convertTotal2PW <- function(df, sizeAssoc, trtName1, trtName2, trtUnit = "percen
     ungroup() %>% 
     updateIDs(trait.directory) %>% 
     filter(traitValue <= 100)
-}
-
-# For cleaning concatenated strings
-cleanStrings <- function(A){
-  A <- str_split(A, pattern =";", simplify = TRUE) 
-  A <- str_trim(A)
-  A <- sort(A)
-  A <- str_replace(A, "NA", replacement = "")
-  A <- A[A != '']
-  A <- unique(as.list(A))
-  A <- paste(A, collapse = "; ")
-  A <- as.character(A)
-}
-cleanStringsList <- function(A){
-  A <- lapply(A, cleanStrings)
-}
-
-cleanScientificName <- function(name){
-  # remove trailing *sp., ?-. symbols, and anything inside parenthesis
-  # remove trailing life stage information
-  name <- gsub(" aff\\.","", name)
-  name <- gsub(" cf\\.","", name)
-  name <- gsub(" C4| C5| CI| CIV| CV| III| IV| VI| NI| NII| NIII| NIV| NV","",name)
-  name <- str_replace(name, "\\s*\\([^\\)]+\\)", "")
-  name <- str_replace_all(name, "[[:punct:]]", "")
-  name <- str_replace_all(name, "[:digit:]", "")
-  name <- gsub(" sp$","", name)
-  name <- gsub(" spp$","", name)
-  name <- gsub(" V$","", name)
-  name <- str_replace(name, " female","")
-  name <- str_replace(name, " male","")
-  name <- str_replace(name, "Agg","")
-  name <- str_replace(name, "aggregate","")
-  name <- str_replace(name, "solitary","")
-  name <- str_squish(str_trim(name))
-  name
 }
 
 # Assign major group based on a row/s of taxonomy information
