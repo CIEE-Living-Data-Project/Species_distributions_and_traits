e137bcc7afdfbabda58bd6113f54409cd96d6351
Samantha Straus, PhD <straus@zoology.ubc.ca>
2023-02-22 14:57:21 -0800

    Update mapping_nitrogen.R

--- mapping_nitrogen.R
+++ mapping_nitrogen.R
@@ -3,6 +3,19 @@ library(tidyverse)
 
 physics.merge <- left_join(physics.bottle, physics.meta, by = "NetKey")
 
+physics.merge <- physics.merge %>% 
+mutate(Season = "Winter") %>% 
+  mutate(Season = if_else(Month >= 3 & Month <=5, "Spring", Season),
+         Season = if_else(Month >= 6 & Month <=8, "Summer", Season),
+         Season = if_else(Month >= 9 & Month <=11, "Fall", Season))
+
+physics.summarizeN <- physics.merge %>% 
+  group_by(Station, Season) %>% 
+  summarize(mean.N = mean(NitrI50),
+            Latitude = mean(Latitude),
+            Longitude = mean(Longitude))
+  
+physics.summarizeN <- left_join(physics.summarizeN, physics.merge, by = "Station")
 
 # PolySet data frames for mapping the Strait of Georgia from the PBSmapping package
 data(nepacLL)
@@ -21,7 +34,18 @@ SOGmap <- ggplot() +
 
 
 # Spatial distribution of samples. Note that there are multiple overlapping points and there are a few stations that are regularly sampled.
+library(viridis)
 SOGmap +
-  geom_point(data = filter(physics.merge, (!is.na(CTDKey.x))), aes(x = Longitude, y = Latitude, color = "Nitrl50"),
-             alpha = 0.5) +
+  geom_point(data = physics.summarizeN, 
+             aes(x = Longitude, y = Latitude, color = mean.N)) +
+  scale_color_viridis()+
+  facet_wrap(~Season)+
   ggtitle("Distribution of Nitrogen")
+
+
+ggplot(data = physics.merge, 
+       aes(x = NetTowDepth, y = NitrI50))+
+  geom_point()+
+  facet_wrap(~Season)
+
+
